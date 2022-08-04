89b866bfa781223beba3b8ac291b8148520fad8a
Владислав Ефимов <notix@yandex.com>
2022-08-04 17:25:49 +0500

    fix after 1st rewiev

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
 router_v1 = DefaultRouter()
-router_v1.register('posts', PostViewSet, basename = 'posts')
+router_v1.register('posts', PostViewSet, basename='posts')
 router_v1.register('groups', GroupViewSet, basename='groups')
 router_v1.register('follow', FollowViewSet, basename='follow')
 router_v1.register(
